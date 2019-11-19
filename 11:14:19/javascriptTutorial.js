5395d7e436817c9d99193748591f3186341d31a8
jmoulton419 <57690540+jmoulton419@users.noreply.github.com>
2019-11-19 18:02:50 -0500

    tts web dev class

---
+++ 11:14:19/javascriptTutorial.js
@@ -0,0 +1,47 @@
+var myFirstString = "We're doing it!"; //string
+var myFirstNumber = 34; //number
+var myFirstBoolean = false; //boolean - aka true or false 
+
+
+console.log(myFirstNumber);
+
+//changing the number
+myFirstNumber = 15;
+
+
+console.log (myFirstNumber);
+
+//equations
+var x = 5; 
+var y = 134;
+var z = 57; 
+
+var sum1 = x * y; 
+console.log(sum1);
+
+var sum2 = (y - (sum1 * z)) / x; 
+console.log(sum2);
+
+if (2 > 7) {
+	console.log('2 is greater than 7');
+} else {
+	console.log('x is greater than y')
+}
+
+if (x.value && y.value && z.value) {
+	console.log('button is clickable');
+	} else {
+		console.log('Please fill in all the required fields')
+	}
+
+	var passwork = 's;lkfsdfj'
+	//Password example
+	if (password.contains('z') || password.contains(4) || password.contains(*)) {
+		console.log('yes');
+	} else if (password.contains (4)) {
+		console.log ('yes');
+	} else if (password.contains(*)) {
+		console.log ('yes');
+	} else {
+		console.log('nope');
+	}
